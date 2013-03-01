7b14659df9c55afddc214089ea3021b2f0b61b46
Ben Babics <bbabics@gmail.com>
2013-02-28 20:49:49 -0500

    jshint cleanup

--- js/subscribers/alerts.js
+++ js/subscribers/alerts.js
@@ -22,5 +22,5 @@ function(facade, controller) {
 
     return {
         init: controller.init
-    }
+    };
 });
--- js/subscribers/menu.js
+++ js/subscribers/menu.js
@@ -13,5 +13,5 @@ function(facade, controller) {
 
     return {
         init: controller.init
-    }
+    };
 });
