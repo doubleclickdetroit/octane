9ff932503483bce339e6445957c4d89cf1ce8f9d
Ben Babics <bbabics@gmail.com>
2013-03-15 11:25:24 -0400

    additional check for window.device so this works in the browser too :)

--- js/models/FeedbackModel.js
+++ js/models/FeedbackModel.js
@@ -11,29 +11,29 @@ function (Backbone, globals, facade, utils) {
         	'sender'         : null,
         	'subject'        : globals.feedback.constants.WEBSERVICE.SUBJECT,
         	'messageBody'    : null,
-        	'devicePlatform' : device.platform || 'Unknown',
-        	'deviceVersion'  : device.version || 'Unknown',
+        	'devicePlatform' : window.device ? device.platform : 'Unknown',
+        	'deviceVersion'  : window.device ? device.version  : 'Unknown',
         	'appBuildVersion': globals.APP.VERSION
         },
-        
+
         // Build the URL for the RESTful web service that receives the Feedback
         url: function () {
-        	
+
         	var url, data;
-        	
+
         	url = globals.feedback.constants.WEBSERVICE.URL;
         	data = this.toJSON();
-        	
+
         	if (data) {
-        	
+
         		// Add the model attributes as query string params to the URL
 	        	url += '?' + $.param(data);
-	        	
+
         	}
-        	
+
         	return url;
         },
-        
+
         validate: function (attrs, options) {
         	// Validate the required fields were supplied
         	if (!attrs.sender && !attrs.messageBody) {
@@ -45,13 +45,13 @@ function (Backbone, globals, facade, utils) {
         	else if (!attrs.messageBody) {
         		return globals.feedback.constants.ERROR_FEEDBACK_REQUIRED_FIELD;
         	}
-        	
+
         	// Validate that sender is in a valid email address format
         	if (!utils.isEmailAddressValid(attrs.sender)) {
         		return globals.feedback.constants.ERROR_EMAIL_INVALID;
         	}
         },
-        
+
         /*
          * Private Methods
         */
@@ -59,7 +59,7 @@ function (Backbone, globals, facade, utils) {
     		model.set(model.defaults);
     		facade.publish('feedback', 'saveSuccess');
         },
-        
+
         /*
          * Public Methods
         */
@@ -70,10 +70,10 @@ function (Backbone, globals, facade, utils) {
         saveAttributes: function () {
             this.save(this.toJSON(), {success: this.handleSaveSuccess});
         }
-        
+
     });
 
 
     return FeedbackModel;
-	
+
 });
