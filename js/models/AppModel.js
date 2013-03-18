df96f9ec305c9c4c9aec1e9044a5547449cb92ec
Erin Parrill <erinparrill@gmail.com>
2013-03-18 19:22:04 -0400

    Moved the Device ID, Screen Type and App Build Version from InfoModel into AppModel.

--- js/models/AppModel.js
+++ js/models/AppModel.js
@@ -8,20 +8,60 @@ function(_, globals, Backbone) {
     AppModel = Backbone.Model.extend({
 
         defaults: {
-            'available': null,
-            'name'     : null,
-            'platform' : 'Unknown',
-            'version'  : 'Unknown',
-            'model'    : null,
-            'uuid'     : null,
-            'cordova'  : null
+            'available'   : null,
+            'name'        : null,
+            'platform'    : 'Unknown',
+            'version'     : 'Unknown',
+            'model'       : null,
+            'uuid'        : null,
+            'cordova'     : null,
+            'deviceId'    : 'Unknown',
+            'screenType'  : 'Unknown',
+            'buildVersion': 'Unknown'
         },
 
         initialize: function() {
             var d = window.device || {};
 
             // bootstrap device data
-            this.set(_.extend(d));
+            this.set(_.extend(d, {
+            	'deviceId'    : this.getDeviceId(),
+            	'screenType'  : this.getScreenType(),
+            	'buildVersion': globals.APP.VERSION
+            }));
+        },
+        
+        /*
+         * Private Methods
+        */
+        
+        getDeviceId: function () {
+            if (typeof window.MyCls !== 'undefined' && window.MyCls.getDeviceId() !== null) {
+                return window.MyCls.getDeviceId();
+            }
+        },
+
+        getScreenType: function () {
+            var screenType = 'Unknown';
+
+            if (typeof window.MyCls !== 'undefined' && window.MyCls.getScreenDensity() !== null) {
+                switch (window.MyCls.getScreenDensity().toString()) {
+                    case '.75':
+                        screenType = 'ldpi';
+                        break;
+                    case '1':
+                        screenType = 'mdpi';
+                        break;
+                    case '1.5':
+                        screenType = 'hdpi';
+                        break;
+                    case '2':
+                        screenType = 'xdpi';
+                        break;
+                }
+            }
+
+            return screenType;
         }
 
     });
--- js/models/InfoModel.js
+++ js/models/InfoModel.js
@@ -15,56 +15,19 @@ function (Backbone, globals) {
         },
 
         initialize: function (attrs, options) {
+            var v = options.device.version,
+            	p = options.device.platform;
+       	
             // initially bootstrap data
             this.set({
-                'buildVersion': globals.APP.VERSION,
-                'osVersion'   : this.getOsVersion(options.device),
-                'deviceId'    : this.getDeviceId(),       // can 'MyCls' be encapsulated within AppModel? If so, these could be as simple as referencing options.device for the property or method that uses 'MyCls' plug-in.
-                'screenType'  : this.getScreenType()      // can 'MyCls' be encapsulated within AppModel? If so, these could be as simple as referencing options.device for the property or method that uses 'MyCls' plug-in.
+                'buildVersion': options.device.buildVersion,
+                'deviceId'    : options.device.deviceId,
+                'screenType'  : options.device.screenType,
+                'osVersion'   : p && v ? p + ' ' + v : 'Unknown'
             });
-        },
-
-        /*
-         * Private Methods
-        */
-        getOsVersion: function (device) {
-        	var v = device.version,
-            	p = device.platform;
-        	
-        	return p && v ? p+' '+v : 'Unknown';
-        },
-        
-        getDeviceId: function () {
-            if (typeof window.MyCls !== 'undefined' && window.MyCls.getDeviceId() !== null) {
-                return window.MyCls.getDeviceId();
-            }
-        },
-
-        getScreenType: function () {
-            var screenType = 'Unknown';
-
-            if (typeof window.MyCls !== 'undefined' && window.MyCls.getScreenDensity() !== null) {
-                switch (window.MyCls.getScreenDensity().toString()) {
-                    case '.75':
-                        screenType = 'ldpi';
-                        break;
-                    case '1':
-                        screenType = 'mdpi';
-                        break;
-                    case '1.5':
-                        screenType = 'hdpi';
-                        break;
-                    case '2':
-                        screenType = 'xdpi';
-                        break;
-                }
-            }
-
-            return screenType;
         }
-
+        
     });
-
 
     return InfoModel;
 
