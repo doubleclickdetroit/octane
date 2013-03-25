0200939f312515e10fb5704def1baf9501026e1d
Ben Babics <bbabics@gmail.com>
2013-03-25 17:54:35 -0400

    Slight refactor to make fat model with LocationHelper. yay.

--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -18,24 +18,14 @@ function(globals, utils, LocationModel, BackboneModel, SearchModel, SearchView) 
 
         SearchController.prototype.init = function() {
             // create model & view instances
-            searchViewModel = new SearchModel();
+            searchViewModel = new SearchModel({
+                locationHelper: LocationModel
+            });
+
             searchView = new SearchView({
                 model: searchViewModel
             });
         };
-
-        /*
-         * Helper Methods
-        */
-        function getCurrentLocation() {
-            var deferred = $.Deferred();
-
-            new LocationModel()
-                .locateFromCurrentLocation()       // fetch location
-                .once('change', deferred.resolve); // resolve with data
-
-            return deferred.promise();             // return promisary-object
-        }
 
         /*
          * Public Methods
@@ -56,53 +46,11 @@ function(globals, utils, LocationModel, BackboneModel, SearchModel, SearchView) 
         };
 
         SearchController.prototype.updateAttribute = function(key, val) {
-            // determine if attribute can immediately be updated
-            // or warrants updating the location attribute as well
-            if (key === globals.search.constants.NAME_SEARCH_BY) {
-                this.updateLocationAndSearchByAttributes(val); // searchBy and location attributes
-            }
-            else {
-                searchViewModel.set(key, val);                 // all other attributes
-            }
+            searchViewModel.set(key, val);
         };
 
         SearchController.prototype.saveSearchCriteriaModel = function() {
             console.log('saveSearchCriteriaModel', searchViewModel.save());
-        };
-
-        /*
-         * Private Methods
-        */
-        SearchController.prototype.updateLocationAndSearchByAttributes = function(searchByValue) {
-            // update searchViewModel with searchBy & location attributes
-            var attributes = {
-                'searchBy': searchByValue
-            };
-
-            // if enterLocation then it should set searchBy
-            if (searchByValue === globals.search.constants.SEARCH_BY_ENTER_LOCATION) {
-                searchViewModel.set(attributes);
-            }
-
-            // if currentLocation then set searchBy and get the current location
-            if (searchByValue === globals.search.constants.SEARCH_BY_CURRENT_LOCATION) {
-                this.loadingBegin();                            // start loading indicator
-
-                $.when(getCurrentLocation())                    // deferred for current location
-                 .then(function(model) {
-                    utils._.extend(attributes, model.toJSON()); // extend attributes with new data
-                    searchViewModel.set(attributes);            // update searchViewModel attributes
-                 })
-                 .done(this.loadingEnd);                        // start loading indicator
-            }
-        };
-
-        SearchController.prototype.loadingBegin = function(checkView) {
-            searchView.showLoadingIndicator(true);
-        };
-
-        SearchController.prototype.loadingEnd = function() {
-            searchView.hideLoadingIndicator(true);
         };
 
         return SearchController;
--- js/models/SearchModel.js
+++ js/models/SearchModel.js
@@ -7,6 +7,24 @@ function(globals, utils, Backbone) {
     var SearchModel;
     SearchModel = (function(_super) {
 
+        var LocationHelper;
+
+        /*
+         * Helper Methods
+        */
+        function getCurrentLocation() {
+            var deferred = $.Deferred();
+
+            new LocationHelper()
+                .locateFromCurrentLocation()               // fetch location
+                .once('change', function() {
+                    console.log('change', arguments);
+                    deferred.resolveWith(this, arguments); // resolve with data
+                }, this);
+
+            return deferred.promise();                     // return promisary-object
+        }
+
         /***********************************************************************
          * Constructor
         ***********************************************************************/
@@ -16,8 +34,18 @@ function(globals, utils, Backbone) {
             SearchModel.__super__.constructor.apply(this, arguments);
         }
 
-        SearchModel.prototype.initialize = function() {
-            this.on('change:searchBy', this.handleLocationAndSearchByAttributes, this);
+        SearchModel.prototype.initialize = function(options) {
+            LocationHelper = options.locationHelper;
+
+            // attribute change dispatcher
+            this.on('change:searchBy', function(model, searchBy) {
+                if (searchBy === globals.search.constants.SEARCH_BY_ADDRESS) {
+                    this.respondToSearchByAddress();
+                }
+                if (searchBy === globals.search.constants.SEARCH_BY_CURRENT_LOCATION) {
+                    this.respondToSearchByLocation();
+                }
+            }, this);
         };
 
         SearchModel.prototype.validate = function() {
@@ -30,14 +58,24 @@ function(globals, utils, Backbone) {
             // override to prevent from trying to UPDATE server
         };
 
-        SearchModel.prototype.handleLocationAndSearchByAttributes = function(model, searchBy) {
-            if (searchBy === globals.search.constants.SEARCH_BY_ENTER_LOCATION) {
-                this.set({
-                    'location' : '',
-                    'latitude' : null,
-                    'longitude': null
+        SearchModel.prototype.respondToSearchByAddress = function() {
+            this.set({
+                'location' : '',
+                'latitude' : null,
+                'longitude': null
+            });
+        };
+
+        SearchModel.prototype.respondToSearchByLocation = function() {
+            this.trigger('loadingbegin');             // start loading indicator
+
+            utils.when(getCurrentLocation.call(this)) // deferred for current location with context
+                .then(function(model) {
+                    this.set(model.toJSON());         // update searchViewModel attributes
+                })
+                .done(function() {
+                    this.trigger('loadingend');       // stop loading indicator
                 });
-            }
         };
 
         return SearchModel;
